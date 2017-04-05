5246514ae7d28a59bb1ee2d92a49fabda678cd13
Ismael Elshazli <ismaelelshazli@hotmail.de>
2017-04-05 11:33:02 +0200

    Add files via upload

---
+++ CPUBurst100.java
@@ -0,0 +1,19 @@
+
+public class CPUBurst100 {
+	
+	public static void main(String [ ] args)
+	{
+		      int x; 
+		      x=1;   
+		      while (x <= 100) {   
+		          System.out.println (Sin(45.0));                    
+		      }   
+		    
+	}
+	public static double Sin(double x) {
+        return Math.toRadians(x);
+        
+	}
+
+ 
+}
